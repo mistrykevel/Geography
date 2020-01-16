971c0df97073c1688fc2111d45e838afbeb388e8
mistrykevel <mistrykevel@gmail.com>
2020-01-16 13:26:45 -0500

    Added questions

--- Multiple choice questions/rng.js
+++ Multiple choice questions/rng.js
@@ -1,63 +1,45 @@
 function generate(){
-    b = document.getElementById("number").innerHTML = Math.floor(Math.random() * 20) + 1
+    b = document.getElementById("number").innerHTML = Math.floor(Math.random() * 14) + 1
     if(b == 1){
-    a = document.getElementById("questions").innerHTML = "What is an example of a renewable resource?" + "<br/>" + "a)Coal" + "<br/>" + "b)Water" + "<br/>" + "c)Soil" + "<br/>" + "d)Sunlight";
+    a = document.getElementById("questions").innerHTML = "What is an example of a renewable resource?" + "<br/>" + "a)Coal" + "<br/>" + "b)Water" + "<br/>" + "c)Soil" + "<br/>" + "d)Sunlight";/*soil*/
     }
     else if(b == 2){
-    a = document.getElementById("questions").innerHTML = "What is an example of a non-renewable resource?" + "<br/>" + "a)Fish" + "<br/>" + "b)Wood" + "<br/>" + "c)Onion" + "<br/>" + "d)Oil"
+    a = document.getElementById("questions").innerHTML = "What is an example of a non-renewable resource?" + "<br/>" + "a)Fish" + "<br/>" + "b)Wood" + "<br/>" + "c)Onion" + "<br/>" + "d)Oil"/*oil*/
     }
     else if(b == 3){
-    a = document.getElementById("questions").innerHTML = "What is an example of a flow resource?" + "<br/>" + "a)Fossil fuels" + "<br/>" + "b)Sun" + "<br/>" + "c)Sunlight" + "<br/>" + "d)Food"
+    a = document.getElementById("questions").innerHTML = "What is an example of a flow resource?" + "<br/>" + "a)Fossil fuels" + "<br/>" + "b)Sun" + "<br/>" + "c)Sunlight" + "<br/>" + "d)Food"/*sunlight*/
     }
     else if(b == 4){
-    a = document.getElementById("questions").innerHTML = "" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If the population growth of a country is 0.4% what is the doubling time?" + "<br/>" + "a)183" + "<br/>" + "b)175" + "<br/>" + "c)107.83" + "<br/>" + "d)146.87"/*175*/
     }
     else if(b == 5){
-    a = document.getElementById("questions").innerHTML = "e" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If there are 10 000 births in a country that has a population of 1 567 895, what is the birth rate?" + "<br/>" + "a)5.05" + "<br/>" + "b)7.37" + "<br/>" + "c)6.38" + "<br/>" + "d)12.94"/*6.38*/
     }
     else if(b == 6){
-    a = document.getElementById("questions").innerHTML = "f" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If there are 5000 deaths in a country that has a population of 1 537 583 what is the death rate?" + "<br/>" + "a)3.25" + "<br/>" + "b)4.49" + "<br/>" + "c)2.67" + "<br/>" + "d)5.28"/*3.25*/
     }
     else if(b == 7){
-    a = document.getElementById("questions").innerHTML = "g" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If the birth rate of a country is 8.46 and the death rate of a country is 5.49, what is the natural increase rate?" + "<br/>" + "a)3.14" + "<br/>" + "b)1.14" + "<br/>" + "c)3.26" + "<br/>" + "d)2.97"/*2.97*/
     }
     else if(b == 8){
-    a = document.getElementById("questions").innerHTML = "h" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If there are 58 305 immigrants coming to a country of 1 593 502 people, what is the immigration rate?" + "<br/>" + "a)34.76" + "<br/>" + "b)36.59" + "<br/>" + "c)38.32" + "<br/>" + "d)35.73"/*36.59*/
     }
     else if(b == 9){
-    a = document.getElementById("questions").innerHTML = "i" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If there are 20 439 people leaving a country of 1 583 053, what is the death rate?" + "<br/>" + "a)11.83" + "<br/>" + "b)12.62" + "<br/>" + "c)12.62" + "<br/>" + "d)12.91"/*12.91*/
     }
     else if(b == 10){
-    a = document.getElementById("questions").innerHTML = "j" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If the immigration rate is 25.26 and the emigration rate is 19.38, what is the net migration rate" + "<br/>" + "a)5.88" + "<br/>" + "b)5.20" + "<br/>" + "c)4.84" + "<br/>" + "d)6.52"/*5.88*/
     } 
     else if(b == 11){
-    a = document.getElementById("questions").innerHTML = "k" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "If the natural increase rate is 4.26 and the net migration rate is 2.72, what is the population growth rate?" + "<br/>" + "a)9.29" + "<br/>" + "b)8.73" + "<br/>" + "c)7.26" + "<br/>" + "d)6.98"/*6.98*/
     }
     else if(b == 12){
-    a = document.getElementById("questions").innerHTML = "l" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "How many points does it take to get into Canada?" + "<br/>" + "a)63" + "<br/>" + "b)67" + "<br/>" + "c)58" + "<br/>" + "d)71"/*67*/
     }
     else if(b == 13){
-    a = document.getElementById("questions").innerHTML = "m" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
+    a = document.getElementById("questions").innerHTML = "Which of these are NOT an environmental approach?" + "<br/>" + "a)Development approach" + "<br/>" + "b)Sustainablilty approach" + "<br/>" + "c)Preservationist approach" + "<br/>" + "d)Conservationist approach"/*sustain*/
     }
     else if(b == 14){
-    a = document.getElementById("questions").innerHTML = "n" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    }    
-    else if(b == 15){
-    a = document.getElementById("questions").innerHTML = "o" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    } 
-    else if(b == 16){
-    a = document.getElementById("questions").innerHTML = "p" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    }
-    else if(b == 17){
-    a = document.getElementById("questions").innerHTML = "q" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    }  
-    else if(b == 18){
-    a = document.getElementById("questions").innerHTML = "r" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    } 
-    else if(b == 19){
-    a = document.getElementById("questions").innerHTML = "s" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    } 
-    else if(b == 20){
-    a = document.getElementById("questions").innerHTML = "t" + "<br/>" + "a)" + "<br/>" + "b)" + "<br/>" + "c)" + "<br/>" + "d)"
-    } 
+    a = document.getElementById("questions").innerHTML = "What is the middle layer of soil?" + "<br/>" + "a)Topsoil" + "<br/>" + "b)Subsoil" + "<br/>" + "c)Parent Material" + "<br/>" + "d)Bedrock"
+    }     
 };
--- True or false questions/rng.js
+++ True or false questions/rng.js
@@ -1,33 +1,51 @@
 function generate(){
-    b = document.getElementById("number").innerHTML = Math.floor(Math.random() * 10) + 1
+    b = document.getElementById("number").innerHTML = Math.floor(Math.random() * 16) + 1
     if(b == 1){
-    a = document.getElementById("questions").innerHTML = "Open space takes up 10% of total land." + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Open space takes up 10% of total land." + "<br/>" + "True or False"/*false*/
     }
     else if(b == 2){
-    a = document.getElementById("questions").innerHTML = "This is a land use co-operation." + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "A land use cooperation is when 2 or more types of land use can prosper together" + "<br/>" + "True or False"/*true*/
     }
     else if(b == 3){
-    a = document.getElementById("questions").innerHTML = "Air is a flow resource." + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Air is a flow resource." + "<br/>" + "True or False"/*true*/
     }
     else if(b == 4){
-    a = document.getElementById("questions").innerHTML = "The greenbelt does not help with protecting the environment" + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "The greenbelt does not help with protecting the environment" + "<br/>" + "True or False"/*false*/
     }
     else if(b == 5){
-    a = document.getElementById("questions").innerHTML = "Wood is a non-renewable resource." + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Wood is a non-renewable resource." + "<br/>" + "True or False"/*false*/
     }
     else if(b == 6){
-    a = document.getElementById("questions").innerHTML = "Fish are a renewable resource." + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Fish are a renewable resource." + "<br/>" + "True or False"/*true*/
     }
     else if(b == 7){
-    a = document.getElementById("questions").innerHTML = "Street trees are a type of land use" + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Street trees are a type of land use" + "<br/>" + "True or False"/*true*/
     }
     else if(b == 8){
-    a = document.getElementById("questions").innerHTML = "h" + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Alberta has the most immigrants" + "<br/>" + "True or False"/*false*/
     }
     else if(b == 9){
-    a = document.getElementById("questions").innerHTML = "i" + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Erosion is the breaking of rocks" + "<br/>" + "True or False"/*false*/
     }
     else if(b == 10){
-    a = document.getElementById("questions").innerHTML = "j" + "<br/>" + "True or False"
+    a = document.getElementById("questions").innerHTML = "Climate change is the rapid heating/cooling of the Earth's temperature" + "<br/>" + "True or False"/*true*/
     } 
+    else if(b == 11){
+    a = document.getElementById("questions").innerHTML = "Permafrost is one of Canada's vegetation regions" + "<br/>" + "True or False"/*false*/
+    }
+    else if(b == 12){
+    a = document.getElementById("questions").innerHTML = "A pine tree is a coniferous tree" + "<br/>" + "True or False"/*true*/
+    }
+    else if(b == 13){
+    a = document.getElementById("questions").innerHTML = "Adaptation is when you try to prevent climate change" + "<br/>" + "True or False"/*false*/
+    }
+    else if(b == 14){
+    a = document.getElementById("questions").innerHTML = "A boom is when a mining town turns into a ghost town beause the mine runs dry" + "<br/>" + "True or False"/**/
+    }
+    else if(b == 15){
+    a = document.getElementById("questions").innerHTML = "Mitigation is when you try and prevent climate change" + "<br/>" + "True or False"/*true*/
+    }
+    else if(b == 16){
+    a = document.getElementById("questions").innerHTML = "A bust is when a mining town's mine runs out of materials and the town turns into a ghost town" + "<br/>" + "True or False"/*true*/
+    }
 }
